6f5c10c7f1d51add9be435ddab2876ff207131bb
Gabriel de Jesus <gabrieldjesus475@gmail.com>
2024-08-13 12:06:03 -0300

    feat: person-controller implemented

---
+++ src/controllers/person-controller.ts
@@ -0,0 +1,34 @@
+import { PersonService } from './../services/person-service';
+import { Request, Response, Router } from 'express';
+
+export class PersonController {
+  private personService: PersonService;
+  private Router: Router;
+
+  constructor(personService: PersonService) {
+    this.personService = personService;
+    this.Router = Router();
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes(): void {
+    this.Router.get('/', this.findAll.bind(this));
+    this.Router.post('/', this.save.bind(this));
+  }
+
+  public getRouter(): Router {
+    return this.Router;
+  }
+
+  public findAll(req: Request, res: Response): void {
+    const persons = this.personService.findAll();
+    res.send(persons);
+  }
+
+  public save(req: Request, res: Response): void {
+    const { name, age } = req.body;
+    this.personService.save(name, age);
+    res.send('Person saved successfully');
+  }
+
+}
