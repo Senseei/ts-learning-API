34529a28223a43657e5d3895dccbe32242c46848
Gabriel de Jesus <gabrieldjesus475@gmail.com>
2024-08-14 16:57:13 -0300

    refact: index.ts is now app.ts for separate concerns

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
 import  express from 'express';
-import { PersonController } from './controllers/person-controller';
+import router from './routers/index';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-const personController = new PersonController();
-
-app.use('/api/persons', personController.getRouter());
+app.use('/api', router);
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
