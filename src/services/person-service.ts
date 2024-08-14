5daeb8710255ba88534e6ea69fb3655aa2c1ec2f
Gabriel de Jesus <gabrieldjesus475@gmail.com>
2024-08-14 16:54:51 -0300

    refact: service is now injectable

--- src/services/person-service.ts
+++ src/services/person-service.ts
@@ -1,19 +1,20 @@
+import { injectable, inject } from 'tsyringe';
 import { Person } from '../models/person';
-import { PersonRepository } from '../repositories/person-repository';
+import { Repository } from '../interfaces/repository';
 
+@injectable()
 export class PersonService {
-    private personRepository: PersonRepository;
     
-    constructor() {
-        this.personRepository = new PersonRepository();
-    }
+    constructor(
+        @inject('PersonRepository') private repository: Repository<Person>
+    ) {}
     
     public save(name: string, age: number): void {
         const person = new Person(name, age);
-        this.personRepository.save(person);
+        this.repository.save(person);
     }
     
     public findAll(): Person[] {
-        return this.personRepository.findAll();
+        return this.repository.findAll();
     }
 }
